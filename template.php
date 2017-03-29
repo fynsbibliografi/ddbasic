3961a31b98d8b9a52a4815a79c8a942cb8cafc95
Tobias F. Petersen <tobsf@hotmail.com>
2017-03-29 09:29:01 +0200

    add huskeliste link to template

--- template.php
+++ template.php
@@ -910,8 +910,9 @@ function ddbasic_menu_link__menu_tabs_menu($vars) {
 
       break;
     case (strpos($element['#href'], 'bookmark') !== false):
+         $title_prefix = '<i class="icon-align-justify"></i>';
          $element['#localized_options']['attributes']['class'][] = 'js-topbar-link';
-         $element['#attributes']['class'] = array();
+         //$element['#attributes']['class'] = array();
       break;
         
     default:
